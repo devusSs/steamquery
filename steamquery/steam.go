ae6be0493b1bc12aadbaf893f3a64a64f736d567
devusSs <flofest98@gmail.com>
2023-05-10 15:56:16 +0200

    improved steam status check

--- steamquery/steam.go
+++ steamquery/steam.go
@@ -8,8 +8,14 @@ import (
 	_ "time/tzdata"
 )
 
+type steamStatus int
+
 const (
 	statusAPIURL = "https://api.steampowered.com/ICSGOServers_730/GetGameServersStatus/v1/?key="
+
+	steamNormal  steamStatus = iota
+	steamDelayed steamStatus = iota
+	steamDown    steamStatus = iota
 )
 
 // Actual check on the Steam API for status of CSGO servers.
@@ -26,5 +32,24 @@ func isSteamCSGOAPIUp(cfg *config) (bool, error) {
 		return false, err
 	}
 
-	return resp.Result.Services.SessionsLogon == "normal" && resp.Result.Services.SteamCommunity == "normal", nil
+	var steamStatusSessions steamStatus
+	var steamStatusCommunity steamStatus
+
+	if resp.Result.Services.SessionsLogon == "normal" {
+		steamStatusSessions = steamNormal
+	} else if resp.Result.Services.SessionsLogon == "delayed" {
+		steamStatusSessions = steamDelayed
+	} else {
+		steamStatusSessions = steamDown
+	}
+
+	if resp.Result.Services.SteamCommunity == "normal" {
+		steamStatusCommunity = steamNormal
+	} else if resp.Result.Services.SteamCommunity == "delayed" {
+		steamStatusCommunity = steamDelayed
+	} else {
+		steamStatusCommunity = steamDown
+	}
+
+	return steamStatusSessions < 3 && steamStatusCommunity < 3, nil
 }
